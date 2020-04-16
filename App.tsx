5c23274d8254b7abe8fefcf3dc51a201af255565
Seungbin Oh <sboh1214@gmail.com>
2020-04-16 20:30:00 +0900

    add realm database

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Realm from 'realm';
 import FeedScreen from 'screens/FeedScreen';
 import SearchScreen from 'screens/SearchScreen';
 import FollowingScreen from 'screens/FollowingScreen';
 import SearchListScreen from 'screens/SearchListScreen';
+import React, {useState, useEffect} from 'react';
 
 const FeedStack = createStackNavigator();
 function FeedStackScreen() {
@@ -39,6 +40,19 @@ function FollowingStackScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [realm, setRealm] = useState<Object | null>(null);
+
+  useEffect(() => {
+    Realm.open({
+      schema: [{name: 'Dog', properties: {name: 'string'}}],
+    }).then((result: any) => {
+      setRealm(result);
+    });
+    return () => {
+      realm?.close();
+    };
+  }, [realm]);
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
